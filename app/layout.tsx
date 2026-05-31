06dafd8a418e066bac948e450d0c16c645708055
xkintaro <mustafatasal4242@gmail.com>
2026-05-31 17:04:01 +0300

    All junk code has been cleaned up.

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,14 +9,14 @@ const inter = Inter({
   display: "swap",
 });
 
-export const dmSans = DM_Sans({
+const dmSans = DM_Sans({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600"],
   variable: "--font-dm-sans",
   display: "swap",
 });
 
-export const playfairDisplay = Playfair_Display({
+const playfairDisplay = Playfair_Display({
   subsets: ['latin'],
   weight: ['400', '600', '700'],
   variable: '--font-playfair-display',
