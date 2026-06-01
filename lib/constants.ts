85ac7238071ba8ad26d9f6cb271f626e7ca459b1
xkintaro <mustafatasal4242@gmail.com>
2026-06-01 22:31:45 +0300

    changed graduation video URL

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,6 +3,6 @@ export const SCHOOL_NAME = "Tunceli Türk Telekom Fen Lisesi";
 export const SCHOOL_DESCRIPTION =
   "2022-2026 mezunları olarak, bu güzel okulda geçirdiğimiz dört yılın anılarını bir araya getirdik. Her birlikte geçirdiğimiz an, her paylaştığımız gülümseme ve her aştığımız zorluk bizi bugünlere getirdi. Bu site, o unutulmaz günlerin bir hatırası olarak kalacak.";
 
-export const GRADUATION_VIDEO_URL = "https://www.youtube.com/embed/s6OqJZSmgvo";
+export const GRADUATION_VIDEO_URL = "https://www.youtube.com/embed/po7bxx89sII";
 
 export const GRADUATION_VIDEO_TITLE = "12/B 2026";
